3b080a551bb79bd050f478dece8ea041453bb14b
ysh702 <177661393+ysh702@users.noreply.github.com>
2026-06-09 23:07:42 +0800

    hdec: compact counter saturating increment logic

--- core/hdec/rtl/hdec_cnt_array.sv
+++ core/hdec/rtl/hdec_cnt_array.sv
@@ -25,34 +25,16 @@ module hdec_cnt_array (
     for (genvar ni = 0; ni < 16; ni++) begin : gen_cnt_nibble
         logic [3:0] old_cnt;
         logic       hv_bit;
-        logic [3:0] inc_one;
+        logic       inc_en;
+        logic [4:0] inc_sum;
 
         assign old_cnt = old_counter_i[4*ni +: 4];
         assign hv_bit  = hv_word_i[bit_base + ni];
-
-        // 4-bit saturating increment by 1 (LUT-style, no arithmetic +)
-        always_comb unique case (old_cnt)
-            4'h0:    inc_one = 4'h1;
-            4'h1:    inc_one = 4'h2;
-            4'h2:    inc_one = 4'h3;
-            4'h3:    inc_one = 4'h4;
-            4'h4:    inc_one = 4'h5;
-            4'h5:    inc_one = 4'h6;
-            4'h6:    inc_one = 4'h7;
-            4'h7:    inc_one = 4'h8;
-            4'h8:    inc_one = 4'h9;
-            4'h9:    inc_one = 4'hA;
-            4'hA:    inc_one = 4'hB;
-            4'hB:    inc_one = 4'hC;
-            4'hC:    inc_one = 4'hD;
-            4'hD:    inc_one = 4'hE;
-            4'hE:    inc_one = 4'hF;
-            4'hF:    inc_one = 4'hF;   // saturated
-            default: inc_one = 4'h0;
-        endcase
+        assign inc_en  = hv_bit & ~&old_cnt;
+        assign inc_sum = {1'b0, old_cnt} + {4'b0, inc_en};
 
         always_comb begin
-            new_counter_o[4*ni +: 4] = hv_bit ? inc_one : old_cnt;
+            new_counter_o[4*ni +: 4] = inc_sum[3:0];
 
             clip_bits_o[ni] = (old_cnt >= clip_threshold_i);
         end
